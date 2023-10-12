f6bcfc45ab84af2e1be187a2413da84bcc07c026
James Whatley <jamesww189@gmail.com>
2023-10-12 09:21:59 -0500

    getting the blue away

---
+++ dTOA.js
@@ -0,0 +1,103 @@
+//let a = 1;
+//let b = a++;
+//let c = ++a;
+// what is the value of a, b, and c?
+// a=3 b=1 c=3
+//console.log(a = 1)
+//console.log(b= a++) //post increment (before the a++ is added +0 *enter +1)
+//console.log(c = ++a) //pre increment (after the ++a is added +1)
+///////////////////
+//let d = "hello";
+//let e = false;
+
+//d++;
+//e++;
+//"hello" false
+//////////////////
+//let perplexed; // perplexed is undefined (no value is assigned)
+//perplexed + 2;
+// undefined
+//////////////////
+//et price = 2.7;
+//price.toFixed(2);
+// 2.70
+//////////////////
+//console.log(let price = "2.7";
+//price.toFixed(2);)
+// false
+//////////////////
+isNaN(0)
+//false
+
+isNaN(1)
+//false
+isNaN("")
+//true
+isNaN("string")
+//true
+isNaN("0")
+//false
+isNaN("1")
+//false
+isNaN("3.145")
+//false
+isNaN(Number.MAX_VALUE)
+//false
+isNaN(Infinity)
+//flase
+isNaN("true")
+//true
+isNaN(true)
+//true
+isNaN("false")
+//true
+isNaN(false)
+//true
+// to illustrate why the isNaN() function is needed:
+NaN == NaN
+//true
+///////////////////////
+
+!true
+//false
+!false
+//true
+!!true
+//true
+!!false
+//false
+!!0
+//false
+!!-0
+//true
+!!1
+//false
+!!-1
+//true
+!!0.1
+//false
+!!"hello"
+//true
+!!""
+//false
+!!''
+//flase
+!!"false"
+//true
+!!"0"
+//true
+//////////////////
+let sample = "hello codeup"
+sample.length // 12
+ sample .toUpperCase //"HELLO CODEUP"
+ sample .toLowerCase(sample)//"hello codeup"
+ sample .substring("hello codeup", "hello codeup students")
+sample .replace(.indexOf("c"))
+sample .replace(.indexOf("C"))
+sample .substring(.indexOf("codeup"))
+
+
+
+
+let username = 'codeup';
+let password = 'notastrongpassword';
---
+++ operators.js
@@ -0,0 +1,39 @@
+'1'+ 2
+//"12"
+""**4
+//0
+typeof '1'+2
+//string2
+6 % 4
+//2
+'3 + 4 is' + 3 + 4
+//'3 + 4 is' + (3 + 4)// 3 + 4 is 7
+0 < 0
+//False
+'false' == false
+//false
+true === 'true'
+//false
+5 >= -5
+//true
+!false || false
+//true
+true || "42"
+// true
+!true && !false
+//false
+6 % 5
+// 1
+5<4 && 1===1
+//false
+typeof 'codeup' === 'string'
+//true
+'codeup' === 'codeup' && 'codeup' === 'Codeup'
+//false
+4>=0 && 1 !=='1'
+//true
+6 % 3 === 0
+//true
+5 % 2 !== 0
+//true
+let a= 1
